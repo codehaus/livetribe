902547d37176df0d6cca1eb3b81afba2ddfd633a
maguro <maguro@6db1a190-aa03-0410-869d-8543fcd43261>
2005-12-11 09:37:32 +0000

    toString() was incorrect.

--- livetribe/trunk/livetribe-jmx/src/main/java/org/livetribe/jmx/MBeanUtils.java
+++ livetribe/trunk/livetribe-jmx/src/main/java/org/livetribe/jmx/MBeanUtils.java
@@ -40,176 +40,182 @@ import javax.management.MBeanOperationInfo;
  */
 public class MBeanUtils
 {
-    private final static Map beanInfoCache = Collections.synchronizedMap( new WeakHashMap() );
+    private final static Map beanInfoCache = Collections.synchronizedMap(new WeakHashMap());
 
-    public static MBeanAttributeInfo[] getMBeanAttributeInfo( Object bean )
+    public final static MBeanAttributeInfo[] getMBeanAttributeInfo(Object bean)
     {
-        return getMBeanAttributeInfo( bean.getClass() );
+        return getMBeanAttributeInfo(bean.getClass());
     }
 
-    public static MBeanConstructorInfo[] getMBeanConstructorInfo( Object bean )
+    public final static MBeanConstructorInfo[] getMBeanConstructorInfo(Object bean)
     {
-        return getMBeanConstructorInfo( bean.getClass() );
+        return getMBeanConstructorInfo(bean.getClass());
     }
 
-    public static MBeanOperationInfo[] getMBeanOperationInfo( Object bean )
+    public final static MBeanOperationInfo[] getMBeanOperationInfo(Object bean)
     {
-        return getMBeanOperationInfo( bean.getClass() );
+        return getMBeanOperationInfo(bean.getClass());
     }
 
-    public static MBeanAttributeInfo[] getMBeanAttributeInfo( Class beanClass )
+    public final static MBeanAttributeInfo[] getMBeanAttributeInfo(Class beanClass)
     {
         MBeanAttributeInfo[] result = new MBeanAttributeInfo[0];
 
         try
         {
-            result = getBeanInfo( beanClass ).attributes;
+            result = getBeanInfo(beanClass).attributes;
         }
-        catch ( IntrospectionException doNothing )
+        catch (IntrospectionException doNothing)
         {
         }
 
         return result;
     }
 
-    public static MBeanConstructorInfo[] getMBeanConstructorInfo( Class beanClass )
+    public final static MBeanConstructorInfo[] getMBeanConstructorInfo(Class beanClass)
     {
         MBeanConstructorInfo[] result = new MBeanConstructorInfo[0];
 
         try
         {
-            result = getBeanInfo( beanClass ).constructors;
+            result = getBeanInfo(beanClass).constructors;
         }
-        catch ( IntrospectionException doNothing )
+        catch (IntrospectionException doNothing)
         {
         }
 
         return result;
     }
 
-    public static MBeanOperationInfo[] getMBeanOperationInfo( Class beanClass )
+    public final static MBeanOperationInfo[] getMBeanOperationInfo(Class beanClass)
     {
         MBeanOperationInfo[] result = new MBeanOperationInfo[0];
 
         try
         {
-            result = getBeanInfo( beanClass ).operations;
+            result = getBeanInfo(beanClass).operations;
         }
-        catch ( IntrospectionException doNothing )
+        catch (IntrospectionException doNothing)
         {
         }
 
         return result;
     }
 
-    private static CacheItem getBeanInfo( Class beanClass ) throws IntrospectionException
+    private static CacheItem getBeanInfo(Class beanClass) throws IntrospectionException
     {
-        CacheItem ci = (CacheItem) beanInfoCache.get( beanClass );
-        if ( ci == null )
+        CacheItem ci = (CacheItem) beanInfoCache.get(beanClass);
+        if (ci == null)
         {
-            BeanInfo bi = Introspector.getBeanInfo( beanClass, Object.class );
+            BeanInfo bi = Introspector.getBeanInfo(beanClass, Object.class);
 
             ci = new CacheItem();
 
-            ci.attributes = getAttributes( bi );
-            ci.constructors = getConstructors( beanClass );
-            ci.operations = getOperations( bi );
+            ci.attributes = getAttributes(bi);
+            ci.constructors = getConstructors(beanClass);
+            ci.operations = getOperations(bi);
 
-            beanInfoCache.put( beanClass, ci );
+            beanInfoCache.put(beanClass, ci);
         }
         return ci;
     }
 
-    private static MBeanAttributeInfo[] getAttributes( BeanInfo bi )
+    private static MBeanAttributeInfo[] getAttributes(BeanInfo bi)
     {
         PropertyDescriptor[] descriptors = bi.getPropertyDescriptors();
         MBeanAttributeInfo[] result = new MBeanAttributeInfo[descriptors.length];
 
-        for ( int i = 0; i < descriptors.length; i++ )
+        for (int i = 0; i < descriptors.length; i++)
         {
             PropertyDescriptor descriptor = descriptors[i];
             boolean hasReadMethod = descriptor.getReadMethod() != null;
-            result[i] = new MBeanAttributeInfo( descriptor.getName(),
-                                                descriptor.getPropertyType().toString(),
-                                                descriptor.getPropertyType() + " " + descriptor.getName(),
-                                                hasReadMethod,
-                                                descriptor.getWriteMethod() != null,
-                                                hasReadMethod && descriptor.getReadMethod().getName().startsWith( "is" ) );
+            result[i] = new MBeanAttributeInfo(descriptor.getName(),
+                                               descriptor.getPropertyType().getName(),
+                                               descriptor.getPropertyType() + " " + descriptor.getName(),
+                                               hasReadMethod,
+                                               descriptor.getWriteMethod() != null,
+                                               hasReadMethod && descriptor.getReadMethod().getName().startsWith("is"));
         }
 
         return result;
     }
 
-    private static MBeanConstructorInfo[] getConstructors( Class beanClass )
+    private static MBeanConstructorInfo[] getConstructors(Class beanClass)
     {
         Constructor[] constructors = beanClass.getConstructors();
         MBeanConstructorInfo[] result = new MBeanConstructorInfo[constructors.length];
 
-        for ( int i = 0; i < constructors.length; i++ )
+        for (int i = 0; i < constructors.length; i++)
         {
-            result[i] = new MBeanConstructorInfo( constructors[i].toString(), constructors[i] );
+            result[i] = new MBeanConstructorInfo(constructors[i].toString(), constructors[i]);
         }
 
         return result;
     }
 
-    private static MBeanOperationInfo[] getOperations( BeanInfo bi )
+    private static MBeanOperationInfo[] getOperations(BeanInfo bi)
     {
         Set methods = new HashSet();
 
         MethodDescriptor[] methodDesc = bi.getMethodDescriptors();
-        for ( int i = 0; i < methodDesc.length; i++ )
+        for (int i = 0; i < methodDesc.length; i++)
         {
-            methods.add( methodDesc[i].getMethod() );
+            methods.add(methodDesc[i].getMethod());
         }
 
         EventSetDescriptor[] eventDesc = bi.getEventSetDescriptors();
-        for ( int i = 0; i < eventDesc.length; i++ )
+        for (int i = 0; i < eventDesc.length; i++)
         {
-            methods.remove( eventDesc[i].getAddListenerMethod() );
-            methods.remove( eventDesc[i].getRemoveListenerMethod() );
-            methods.remove( eventDesc[i].getGetListenerMethod() );
+            methods.remove(eventDesc[i].getAddListenerMethod());
+            methods.remove(eventDesc[i].getRemoveListenerMethod());
+            methods.remove(eventDesc[i].getGetListenerMethod());
         }
 
         PropertyDescriptor[] propDesc = bi.getPropertyDescriptors();
-        for ( int i = 0; i < propDesc.length; i++ )
+        for (int i = 0; i < propDesc.length; i++)
         {
-            methods.remove( propDesc[i].getReadMethod() );
-            methods.remove( propDesc[i].getWriteMethod() );
+            methods.remove(propDesc[i].getReadMethod());
+            methods.remove(propDesc[i].getWriteMethod());
         }
 
         MBeanOperationInfo[] result = new MBeanOperationInfo[methods.size()];
         Iterator iter = methods.iterator();
-        for ( int i = 0; i < result.length; i++ )
+        for (int i = 0; i < result.length; i++)
         {
             Method method = (Method) iter.next();
-            result[i] = new MBeanOperationInfo( method.toString(), method );
+            result[i] = new MBeanOperationInfo(method.toString(), method);
         }
 
         return result;
     }
 
-    public static String capitalize(String name) {
-	if (name == null || name.length() == 0) {
-	    return name;
-	}
-	char chars[] = name.toCharArray();
-	chars[0] = Character.toUpperCase(chars[0]);
-	return new String(chars);
+    public final static String capitalize(String name)
+    {
+        if (name == null || name.length() == 0)
+        {
+            return name;
+        }
+        char chars[] = name.toCharArray();
+        chars[0] = Character.toUpperCase(chars[0]);
+        return new String(chars);
     }
 
-    public static String decapitalize(String name) {
-	if (name == null || name.length() == 0) {
-	    return name;
-	}
-	if (name.length() > 1 && Character.isUpperCase(name.charAt(1)) &&
-			Character.isUpperCase(name.charAt(0))){
-	    return name;
-	}
-	char chars[] = name.toCharArray();
-	chars[0] = Character.toLowerCase(chars[0]);
-	return new String(chars);
+    public final static String decapitalize(String name)
+    {
+        if (name == null || name.length() == 0)
+        {
+            return name;
+        }
+        if (name.length() > 1 && Character.isUpperCase(name.charAt(1)) &&
+            Character.isUpperCase(name.charAt(0)))
+        {
+            return name;
+        }
+        char chars[] = name.toCharArray();
+        chars[0] = Character.toLowerCase(chars[0]);
+        return new String(chars);
     }
+
     private static class CacheItem
     {
         public MBeanAttributeInfo[] attributes;
