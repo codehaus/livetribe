1c9466a486224729a41fa87adad86f9a54326d82
maguro <maguro@6db1a190-aa03-0410-869d-8543fcd43261>
2010-02-01 05:21:09 +0000

    re-org

---
+++ garden/livetribe-bootagent/trunk/tests/server/src/main/java/org/livetribe/boot/server/FileContentProvider.java
@@ -0,0 +1,102 @@
+/**
+ *
+ * Copyright 2008-2010 (C) The original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.livetribe.boot.server;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.livetribe.boot.protocol.BootException;
+import org.livetribe.boot.protocol.ContentProvider;
+
+
+/**
+ * The <code>FileContentProvider</code> is an implementation of
+ * <code>ContentProvider</code> backed by a file system.
+ * <p/>
+ * The first level of directories correspond to the names of the content and so
+ * the directory names match the content names that they represent.  Within
+ * those directories are the collection of versions of that content.  They are of
+ * the format <code><i>version</i>.jar</code>.  For example, content with the
+ * name bar has three versions and the content named foo has two:
+ * <p/>
+ * <blockquote><pre>
+ *     bar/
+ *       1.jar
+ *       2.jar
+ *       3.jar
+ *     foo/
+ *       1.jar
+ *       2.jar
+ * </pre></blockquote>
+ *
+ * @version $Revision$ $Date$
+ */
+public class FileContentProvider implements ContentProvider
+{
+    private final static Logger LOGGER = LoggerFactory.getLogger(FileContentProvider.class);
+    private final File resources;
+
+    /**
+     * @param resources the root directory of the content that this provider
+     *                  delivers.  This directory must already exist and its
+     *                  contents must be in the format described in this class'
+     *                  description.
+     */
+    public FileContentProvider(File resources)
+    {
+        if (resources == null) throw new IllegalArgumentException("Directory cannot be null");
+        if (!resources.exists() || !resources.isDirectory()) throw new IllegalArgumentException("Directory does not exist or is a file");
+
+        LOGGER.trace("resources: {}", resources);
+
+        this.resources = resources;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public InputStream pleaseProvide(String name, long version) throws BootException
+    {
+        LOGGER.trace("pleaseProvide({}, {})", name, version);
+
+        if (name == null) throw new BootException("Content name cannot be null");
+        if (version < 0) throw new BootException("Content version cannot be negative");
+
+        File directory = new File(resources, name);
+
+        if (!directory.exists() || !directory.isDirectory()) throw new BootException("Directory for resource does not exist");
+
+        File resource = new File(directory, Long.toString(version) + ".jar");
+
+        if (!resource.exists() || !resource.isFile()) throw new BootException("Version for resource does not exist");
+
+        try
+        {
+            LOGGER.trace("pleaseProvide = {}", resource.toURL());
+
+            return resource.toURL().openStream();
+        }
+        catch (IOException ioe)
+        {
+            throw new BootException("Unable to open stream for resource", ioe);
+        }
+    }
+}
---
+++ garden/livetribe-bootagent/trunk/tests/server/src/main/java/org/livetribe/boot/server/PropertiesProvisionProvider.java
@@ -0,0 +1,196 @@
+/**
+ *
+ * Copyright 2007-2010 (C) The original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.livetribe.boot.server;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Properties;
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.livetribe.boot.protocol.BootException;
+import org.livetribe.boot.protocol.DoNothing;
+import org.livetribe.boot.protocol.ProvisionDirective;
+import org.livetribe.boot.protocol.ProvisionEntry;
+import org.livetribe.boot.protocol.ProvisionProvider;
+import org.livetribe.boot.protocol.YouMust;
+import org.livetribe.boot.protocol.YouShould;
+
+
+/**
+ * A properties file based provisioning provider.
+ * <p/>
+ * <dl>
+ * <dt>version</dt>
+ * <dd>the version of this provisioning directive</dd>
+ * <dt>boot.class</dt>
+ * <dd>the boot class to instantiate and make lifecycle calls to</dd>
+ * <dt>directive.x</dt>
+ * <dd>a provisioning entry in the format of &lt;name&gt;:&lt;version&gt;.  The order of
+ * these entries, specified by the consecutive numbers ".x" indicate the order
+ * to be used when constructing the classpath when the boot class is
+ * instantiated and started.</dd>
+ * <dt>required</dt>
+ * <dd>indicate whether or not a forced restart should occur.  The default is
+ * <code>false</code></dd>
+ * </dl>
+ * <p/>
+ * Entries that are directed to specific UUIDs are prefixed with that UUID +
+ * ".", e.g. <code>1d02-d3c9.version</code> would be used to specify the
+ * version of the provisioning directive for UUID <code>1d02-d3c9</code>.
+ * <p/>
+ * In the following example the version of the default provisioning directive
+ * is 10, ts boot class is <code>com.acme.boot.BootAgent</code>, and its
+ * provisioning entries are:
+ * <ol>
+ * <li>acme-a, version 1</li>
+ * <li>acme-b, version 1</li>
+ * <li>acme-b, version 1</li>
+ * </ol>
+ * <p/>
+ * For the UUID <code>1d02-d3c9</code> the version of the default provisioning directive
+ * is 5, ts boot class is <code>com.foo.boot.BootAgent</code>, its
+ * provisioning entries are:
+ * <ol>
+ * <li>foo-a, version 4</li>
+ * <li>acme-a, version 1</li>
+ * <li>acme-b, version 2</li>
+ * <li>acme-b, version 1</li>
+ * </ol>
+ * and it is directed to restart once the entries have been loaded.
+ * <p/>
+ * <blockquote><pre>
+ * version=10
+ * boot.class=com.acme.boot.BootAgent
+ * directive.0=acme-a:1
+ * directive.1=acme-b:1
+ * directive.2=acme-c:1
+ * 1d02-d3c9.version=5
+ * 1d02-d3c9.boot.class=com.foo.boot.BootAgent
+ * 1d02-d3c9.directive.0=foo-a:4
+ * 1d02-d3c9.directive.1=acme-a:1
+ * 1d02-d3c9.directive.2=acme-b:2
+ * 1d02-d3c9.directive.3=acme-c:1
+ * 1d02-d3c9.required=true
+ * </pre></blockquote>
+ *
+ * @version $Revision$ $Date$
+ */
+public class PropertiesProvisionProvider implements ProvisionProvider
+{
+    private final static Logger LOGGER = LoggerFactory.getLogger(PropertiesProvisionProvider.class);
+    private final static String REQUIRED_KEY = "required";
+    private final static String RESTART_KEY = "restart";
+    private final static String VERSION_KEY = "version";
+    private final static String BOOT_CLASS_KEY = "boot.class";
+    private final static String DIRECTIVE_BASE_KEY = "directive.";
+    private final Properties properties = new Properties();
+    private final File file;
+
+
+    public PropertiesProvisionProvider(File file) throws IOException
+    {
+        if (file == null) throw new IllegalArgumentException("File cannot be null");
+        if (!file.exists()) throw new IllegalArgumentException("File does not exist");
+
+        this.file = file;
+
+        LOGGER.trace("file: {}", file);
+
+        reload();
+    }
+
+    /**
+     * Reloads the configuration of this provisioning provider.
+     *
+     * @throws IOException if there is an error loading the properties file
+     */
+    public synchronized void reload() throws IOException
+    {
+        properties.clear();
+        properties.load(new FileInputStream(file));
+
+        if (LOGGER.isTraceEnabled())
+        {
+            LOGGER.trace("reload");
+            @SuppressWarnings({"unchecked"}) Enumeration<String> keys = (Enumeration<String>)properties.propertyNames();
+            while (keys.hasMoreElements())
+            {
+                String key = keys.nextElement();
+                LOGGER.trace("{} = {}", key, properties.getProperty(key));
+            }
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public synchronized ProvisionDirective hello(String uuid, long version) throws BootException
+    {
+        LOGGER.trace("hello({}, {})", uuid, version);
+
+        ProvisionDirective directive;
+
+        if (properties.containsKey(uuid + "." + VERSION_KEY)) directive = load(uuid + ".", version);
+        else if (properties.containsKey(VERSION_KEY)) directive = load("", version);
+        else throw new BootException("No directives found for " + uuid);
+
+        LOGGER.trace("hello = {}", directive);
+
+        return directive;
+    }
+
+    protected ProvisionDirective load(String prefix, long version) throws BootException
+    {
+        LOGGER.trace("load({}, {})", prefix, version);
+
+        assert Thread.holdsLock(this);
+
+        final long v = Long.valueOf((String)properties.get(prefix + VERSION_KEY));
+
+        if (v == version) return new DoNothing();
+
+        String bootClass = (String)properties.get(prefix + BOOT_CLASS_KEY);
+        Set<ProvisionEntry> entries = new HashSet<ProvisionEntry>();
+        int count = 0;
+        while (true)
+        {
+            String key = prefix + DIRECTIVE_BASE_KEY + count++;
+
+            if (!properties.containsKey(key)) break;
+
+            String[] tokens = ((String)properties.get(key)).split(":");
+
+            if (tokens.length != 2) throw new BootException("Malformed entry for " + key);
+
+            entries.add(new ProvisionEntry(tokens[0], Long.valueOf(tokens[1])));
+        }
+
+        ProvisionDirective directive;
+        if (properties.containsKey(prefix + REQUIRED_KEY)) directive = new YouMust(v, bootClass, entries, properties.containsKey(prefix + RESTART_KEY));
+        else directive = new YouShould(v, bootClass, entries);
+
+        LOGGER.trace("load = {}", directive);
+
+        return directive;
+    }
+}
