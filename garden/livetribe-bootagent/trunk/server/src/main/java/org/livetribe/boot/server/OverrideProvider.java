7e2343a82cad0bec0507c02dc62551d109c35c13
maguro <maguro@6db1a190-aa03-0410-869d-8543fcd43261>
2010-02-05 21:33:36 +0000

    Filled in details.

--- garden/livetribe-bootagent/trunk/server/src/main/java/org/livetribe/boot/server/OverrideProvider.java
+++ garden/livetribe-bootagent/trunk/server/src/main/java/org/livetribe/boot/server/OverrideProvider.java
@@ -1,6 +1,6 @@
 /**
  *
- * Copyright 2009 (C) The original author or authors
+ * Copyright 2010 (C) The original author or authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -21,6 +21,7 @@ import java.util.logging.Logger;
 
 import org.livetribe.boot.protocol.BootException;
 import org.livetribe.boot.protocol.ContentProvider;
+import org.livetribe.boot.protocol.DoNothing;
 import org.livetribe.boot.protocol.ProvisionDirective;
 import org.livetribe.boot.protocol.ProvisionProvider;
 
@@ -34,21 +35,40 @@ public class OverrideProvider implements ProvisionProvider, ContentProvider
     private final static Logger LOGGER = Logger.getLogger(CLASS_NAME);
     private final ProvisionProvider provisionProvider;
     private final ContentProvider contentProvider;
+    private final ProvisionProvider overrideProvisionProvider;
+    private final ContentProvider overrideContentProvider;
+    private volatile boolean override = false;
 
-    public OverrideProvider(ProvisionProvider provisionProvider, ContentProvider contentProvider)
+    public OverrideProvider(ProvisionProvider provisionProvider, ContentProvider contentProvider, ProvisionProvider overrideProvisionProvider, ContentProvider overrideContentProvider)
     {
         if (provisionProvider == null) throw new IllegalArgumentException("Provision provider is null");
         if (contentProvider == null) throw new IllegalArgumentException("Content provider is null");
+        if (overrideProvisionProvider == null) throw new IllegalArgumentException("Override provision provider is null");
+        if (overrideContentProvider == null) throw new IllegalArgumentException("Override content provider is null");
 
         this.provisionProvider = provisionProvider;
         this.contentProvider = contentProvider;
+        this.overrideProvisionProvider = overrideProvisionProvider;
+        this.overrideContentProvider = overrideContentProvider;
     }
 
     public ProvisionDirective hello(String uuid, long version) throws BootException
     {
         LOGGER.entering(CLASS_NAME, "hello", new Object[]{uuid, version});
 
-        ProvisionDirective directive = provisionProvider.hello(uuid, version);
+        ProvisionDirective directive = overrideProvisionProvider.hello(uuid, version);
+        if (!(directive instanceof DoNothing))
+        {
+            if (!override) LOGGER.info("Provision provider has been overridden");
+            override = true;
+            LOGGER.finer("Provision provider has been overridden");
+        }
+        else
+        {
+            override = false;
+            directive = provisionProvider.hello(uuid, version);
+            LOGGER.finer("Provision provider has not been overridden");
+        }
 
         LOGGER.exiting(CLASS_NAME, "hello", directive);
 
@@ -59,7 +79,17 @@ public class OverrideProvider implements ProvisionProvider, ContentProvider
     {
         LOGGER.entering(CLASS_NAME, "pleaseProvide", new Object[]{name, version});
 
-        InputStream stream = contentProvider.pleaseProvide(name, version);
+        InputStream stream;
+        if (override)
+        {
+            stream = overrideContentProvider.pleaseProvide(name, version);
+            LOGGER.finer("Content provider has been overridden");
+        }
+        else
+        {
+            stream = contentProvider.pleaseProvide(name, version);
+            LOGGER.finer("Content provider has not been overridden");
+        }
 
         LOGGER.exiting(CLASS_NAME, "pleaseProvide", stream);
 
