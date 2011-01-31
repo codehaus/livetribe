445de095e3a84dfe27fc7df5e5d6d27c719610c7
maguro <maguro@6db1a190-aa03-0410-869d-8543fcd43261>
2011-01-31 02:57:44 +0000

    v2.1.2 tag

---
+++ garden/livetribe-slp/tags/2.1.2/core/src/main/java/org/livetribe/slp/spi/filter/ExpressionFilter.java
@@ -0,0 +1,303 @@
+/*
+ * Copyright 2006-2011 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.livetribe.slp.spi.filter;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.livetribe.slp.Attributes;
+import org.livetribe.slp.SLPError;
+import org.livetribe.slp.ServiceLocationException;
+
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class ExpressionFilter implements Filter
+{
+    private static final Pattern EXPRESSION_PATTERN = Pattern.compile("(.+?)([<>]?=)(.+)");
+    private static final Pattern OCTET_STRING_PATTERN = Pattern.compile("(\\\\[\\da-f]{2})+");
+
+    static final String GE = ">=";
+    static final String LE = "<=";
+    static final String EQ = "=";
+    private static final String ANY = "*";
+
+    private final String lhs;
+    private final String operator;
+    private final String rhs;
+
+    public ExpressionFilter(String lhs, String operator, String rhs) throws ServiceLocationException
+    {
+        this.lhs = lhs;
+        this.operator = operator;
+        this.rhs = rhs;
+
+        // Check if wildcard comparison is done properly (RFC 2608, 8.1)
+        if (!EQ.equals(operator) && rhs.indexOf(ANY) >= 0)
+            throw new ServiceLocationException("Invalid filter " + this + ": wildcard matching is only allowed with operator " + EQ, SLPError.PARSE_ERROR);
+    }
+
+    public static ExpressionFilter fromString(String expr) throws ServiceLocationException
+    {
+        if (expr == null)
+        {
+            throw new IllegalArgumentException("expr is null");
+        }
+
+        Matcher matcher = EXPRESSION_PATTERN.matcher(expr);
+        if (matcher.matches())
+        {
+            return new ExpressionFilter(
+                    matcher.group(1),
+                    matcher.group(2),
+                    matcher.group(3)
+            );
+        }
+
+        throw new ServiceLocationException("Bad filter expression: " + expr, SLPError.PARSE_ERROR);
+    }
+
+    public boolean matches(Attributes attributes)
+    {
+        if (attributes == null) return false;
+
+        Attributes.Value value = attributes.valueFor(lhs);
+        if (value == null) return false;
+
+        // Check for presence only
+        if (value.isPresenceType() && EQ.equals(operator) && ANY.equals(rhs)) return true;
+
+        return compare(value, operator, rhs);
+    }
+
+    private boolean compare(Attributes.Value attributeValue, String operator, String compare)
+    {
+        if (attributeValue.isStringType())
+        {
+            if (GE.equals(operator))
+            {
+                return compareStringValue((String)attributeValue.getValue(), compare) >= 0;
+            }
+            else if (LE.equals(operator))
+            {
+                return compareStringValue((String)attributeValue.getValue(), compare) <= 0;
+            }
+            else if (EQ.equals(operator))
+            {
+                boolean result = false;
+                if (compare.indexOf(ANY) >= 0)
+                {
+                    // Wildcard comparison
+                    String[] parts = compare.split("\\*", 0);
+                    Object[] values = attributeValue.getValues();
+                    for (Object value : values)
+                    {
+                        String stringValue = normalizeSpace((String)value).toLowerCase();
+                        boolean match = true;
+                        int start = 0;
+                        for (String part : parts)
+                        {
+                            part = normalizeFilterValue(part).toLowerCase();
+                            if (part.length() > 0)
+                            {
+                                int index = stringValue.indexOf(part, start);
+                                match &= index >= 0;
+                                start = index + 1;
+                            }
+                        }
+                        result |= match;
+                    }
+                }
+                else
+                {
+                    // Direct comparison
+                    Object[] values = attributeValue.getValues();
+                    for (Object value : values)
+                    {
+                        result |= compareStringValue((String)value, compare) == 0;
+                    }
+                }
+                return result;
+            }
+            else
+            {
+                throw new AssertionError("Invalid operator " + operator);
+            }
+        }
+        else if (attributeValue.isIntegerType())
+        {
+            try
+            {
+                int compareInteger = Integer.parseInt(unescape(compare.trim()));
+                if (GE.equals(operator))
+                {
+                    Object[] values = attributeValue.getValues();
+                    boolean result = false;
+                    for (Object value : values)
+                    {
+                        Integer integerValue = (Integer)value;
+                        result |= integerValue >= compareInteger;
+                    }
+                    return result;
+                }
+                else if (LE.equals(operator))
+                {
+                    Object[] values = attributeValue.getValues();
+                    boolean result = false;
+                    for (Object value : values)
+                    {
+                        Integer integerValue = (Integer)value;
+                        result |= integerValue <= compareInteger;
+                    }
+                    return result;
+                }
+                else if (EQ.equals(operator))
+                {
+                    Object[] values = attributeValue.getValues();
+                    boolean result = false;
+                    for (Object value : values)
+                    {
+                        Integer integerValue = (Integer)value;
+                        result |= integerValue == compareInteger;
+                    }
+                    return result;
+                }
+                else
+                {
+                    throw new AssertionError("Invalid operator " + operator);
+                }
+            }
+            catch (NumberFormatException x)
+            {
+                return false;
+            }
+        }
+        else if (attributeValue.isBooleanType())
+        {
+            if (!EQ.equals(operator)) return false;
+            if (!"true".equalsIgnoreCase(unescape(compare)) && !"false".equalsIgnoreCase(unescape(compare))) return false;
+            Boolean value = (Boolean)attributeValue.getValue();
+            return value.equals(Boolean.valueOf(unescape(compare)));
+        }
+        else if (attributeValue.isOpaqueType())
+        {
+            if (!EQ.equals(operator)) return false;
+            byte[] value = (byte[])attributeValue.getValue();
+
+            return Arrays.equals(octetStringToBytes(compare), value);
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    protected static int compareStringValue(String attributeValue, String filterValue)
+    {
+        return normalizeSpace(attributeValue).compareToIgnoreCase(
+                normalizeFilterValue(filterValue)
+        );
+    }
+
+    protected static String normalizeFilterValue(String value)
+    {
+        return normalizeSpace(unescape(value));
+    }
+
+    protected static String normalizeSpace(String s)
+    {
+        return s.trim().replaceAll("\\s+", "");
+    }
+
+    protected static String unescape(String escaped)
+    {
+        Matcher matcher = OCTET_STRING_PATTERN.matcher(escaped);
+
+        StringBuffer unescaped = new StringBuffer();
+
+        while (matcher.find())
+        {
+            matcher.appendReplacement(
+                    unescaped, Matcher.quoteReplacement(octetString2UTF8(matcher.group(0)))
+            );
+        }
+        matcher.appendTail(unescaped);
+
+        return unescaped.toString().trim().replaceAll("\\s+", " ");
+    }
+
+    private static String octetString2UTF8(String ostr) throws ServiceLocationException
+    {
+        try
+        {
+            return new String(octetStringToBytes(ostr), "UTF-8");
+        }
+        catch (UnsupportedEncodingException uee)
+        {
+            throw new ServiceLocationException("Error while decoding octet string: " + ostr, uee, SLPError.PARSE_ERROR);
+        }
+    }
+
+    private static byte[] octetStringToBytes(String ostr) throws ServiceLocationException
+    {
+        if (ostr.length() == 0) return new byte[0];
+
+        if (ostr.length() % 3 != 0)
+        {
+            throw new ServiceLocationException("Illegal escape sequence, number of characters is not multiple of 3",
+                                               SLPError.PARSE_ERROR);
+        }
+
+        int len = ostr.length() / 3;
+
+        byte[] bytes = new byte[len];
+        for (int i = 0; i < len; i++)
+        {
+            if (ostr.charAt(i * 3) != '\\')
+            {
+                throw new ServiceLocationException("Escape sequence does not start with \\ at " + (3 * i) + ": " + ostr,
+                                                   SLPError.PARSE_ERROR);
+            }
+
+            try
+            {
+                bytes[i] = (byte)(
+                        Integer.parseInt(
+                                ostr.substring(3 * i + 1, 3 * i + 3), 16
+                        ) & 0xFF
+                );
+            }
+            catch (NumberFormatException nfe)
+            {
+                throw new ServiceLocationException("Illegal escape sequence at " + (3 * i + 1) + ": " + ostr,
+                                                   SLPError.PARSE_ERROR);
+            }
+        }
+
+        return bytes;
+    }
+
+    public String asString()
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.append("(").append(lhs).append(operator).append(rhs).append(")");
+        return builder.toString();
+    }
+
+}
---
+++ garden/livetribe-slp/tags/2.1.2/core/src/test/java/org/livetribe/slp/spi/filter/FilterTest.java
@@ -0,0 +1,347 @@
+/*
+ * Copyright 2006-2011 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.livetribe.slp.spi.filter;
+
+import org.testng.annotations.Test;
+
+import org.livetribe.slp.Attributes;
+import org.livetribe.slp.ServiceLocationException;
+
+
+/**
+ * @version $Rev$ $Date$
+ */
+public class FilterTest
+{
+    @Test
+    public void testMatchSingleString() throws Exception
+    {
+        String attributeList = "(a=foo)";
+        Attributes attributes = Attributes.from(attributeList);
+        FilterParser parser = new FilterParser();
+
+        Filter filter = parser.parse("(a=Foo)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a >= f)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a >= F)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a<=g)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a=f*o)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a=FO*)");
+        assert filter.matches(attributes);
+    }
+
+    @Test
+    public void testMatchWithWhitespace() throws Exception
+    {
+        String attributeList = "(a=fo  o)";
+        Attributes attributes = Attributes.from(attributeList);
+        FilterParser parser = new FilterParser();
+
+        Filter filter = parser.parse("( a   = Fo  o )");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a >= f)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a >= F)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a<=g)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a=f*o)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a=FO*)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a=fo   o*)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a=fo  o*)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a=fo o*)");
+        assert filter.matches(attributes);
+    }
+
+    @Test
+    public void testMatchSingleLong() throws Exception
+    {
+        String attributeList = "(a=14)";
+        Attributes attributes = Attributes.from(attributeList);
+        FilterParser parser = new FilterParser();
+
+        Filter filter = parser.parse("(a=14)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a >= 10)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a<=17)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a=foo)");
+        assert !filter.matches(attributes);
+    }
+
+    @Test
+    public void testMatchSingleBoolean() throws Exception
+    {
+        String attributeList = "(a=true)";
+        Attributes attributes = Attributes.from(attributeList);
+        FilterParser parser = new FilterParser();
+
+        Filter filter = parser.parse("(a=true)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(!(a=false))");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(a>=false)");
+        assert !filter.matches(attributes);
+
+        filter = parser.parse("(a<=true)");
+        assert !filter.matches(attributes);
+
+        filter = parser.parse("(a=foo)");
+        assert !filter.matches(attributes);
+
+        filter = parser.parse("(a=10)");
+        assert !filter.matches(attributes);
+    }
+
+    @Test
+    public void testMatchSinglePresence() throws Exception
+    {
+        String attributeList = "bar";
+        Attributes attributes = Attributes.from(attributeList);
+        FilterParser parser = new FilterParser();
+
+        Filter filter = parser.parse("(bar=*)");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(foo=*)");
+        assert !filter.matches(attributes);
+    }
+
+    @Test
+    public void testMultipleParenthesis() throws Exception
+    {
+        String attributeList = "(bar=foo)";
+        Attributes attributes = Attributes.from(attributeList);
+        FilterParser parser = new FilterParser();
+
+        Filter filter = parser.parse("(((bar=*)))");
+        assert filter.matches(attributes);
+    }
+
+    @Test
+    public void testMatchMultipleStrings() throws Exception
+    {
+        String attributeList = "(x=true,2,bar)";
+        Attributes attributes = Attributes.from(attributeList);
+        FilterParser parser = new FilterParser();
+
+        Filter filter = parser.parse("(x=bar)");
+        assert filter.matches(attributes);
+    }
+
+    @Test
+    public void testMatchMultipleLongs() throws Exception
+    {
+        String attributeList = "(x=1,2,3)";
+        Attributes attributes = Attributes.from(attributeList);
+        FilterParser parser = new FilterParser();
+
+        Filter filter = parser.parse("(x=2)");
+        assert filter.matches(attributes);
+    }
+
+    @Test
+    public void testMatchMultipleBooleans() throws Exception
+    {
+        String attributeList = "(x=true,false)";
+        Attributes attributes = Attributes.from(attributeList);
+        FilterParser parser = new FilterParser();
+
+        Filter filter = parser.parse("(x=true)");
+        assert filter.matches(attributes);
+    }
+
+    @Test
+    public void testMultipleMatches() throws Exception
+    {
+        String attributeList = "(a=1,2),(b=false),(name=name1)";
+        Attributes attributes = Attributes.from(attributeList);
+        FilterParser parser = new FilterParser();
+
+        Filter filter = parser.parse("(&(a=1)(b=false))");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(&(a=1)(b=true))");
+        assert !filter.matches(attributes);
+
+        filter = parser.parse("(&(!(b=false))(a=1))");
+        assert !filter.matches(attributes);
+
+        filter = parser.parse("(&(!(b=true))(name=name*))");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(|(name=name*)(&(!(b=true))(a=2)))");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(|(name=name*)(&(!(b=true))(a=3)))");
+        assert filter.matches(attributes);
+
+        filter = parser.parse("(|(a=3)(&(!(b=true))))");
+        assert filter.matches(attributes);
+    }
+
+    @Test
+    public void testMatchEscapedString()
+    {
+        FilterParser parser = new FilterParser();
+
+        //Test rfc2254 reserved characters
+        Attributes attributes = Attributes.from("(msg=*\\28\\29\\5c)");
+
+        assert parser
+                .parse("(msg=\\2a\\28\\29\\5c)")
+                .matches(attributes);
+
+        assert parser
+                .parse("(msg=*()\\")
+                .matches(attributes);
+
+        //Test rfc2608.txt reserved characters ( minus rfc2254 reserved ones )
+        assert parser
+                .parse("(msg=,<=>==~!)")
+                .matches(Attributes.from("(msg=\\2c\\3c\\3d\\3e\\3d\\3d\\7e\\21)"));
+
+        //Generic char
+        assert parser
+                .parse("(msg=ab\\63)")
+                .matches(Attributes.from("(msg=abc)"));
+
+        assert parser
+                .parse("(msg=ab\\63*)")
+                .matches(Attributes.from("(msg=abcd)"));
+
+        assert parser
+                .parse("(msg=\\61b\\63)")
+                .matches(Attributes.from("(msg=abc)"));
+
+        //UTF-8 char
+        assert parser
+                .parse("(msg=\\e1\\ba\\a3)")
+                .matches(Attributes.from("(msg=\u1EA3)"));
+
+        //Invalid UTF-8 char
+        assert !parser
+                .parse("(msg=abc\\FFd)")
+                .matches(Attributes.from("(msg=abcd)"));
+
+        //NUL
+        assert parser
+                .parse("(msg=abc\\00qwe)")
+                .matches(Attributes.from("(msg=abc\\00qwe)"));
+
+        //Numeric
+        assert parser
+                .parse("(msg=12\\33)")
+                .matches(Attributes.from("(msg=123)"));
+
+        //Boolean
+        assert parser
+                .parse("(msg=tr\\55e)")
+                .matches(Attributes.from("(msg=true)"));
+
+        //Opaque
+        assert parser
+                .parse("(msg=\\01\\02\\03)")
+                .matches(Attributes.from("(msg=\\FF\\01\\02\\03)"));
+
+        //Broken escape sequence (will be kept as-is)
+        assert parser
+                .parse("(msg=\\qq)")
+                .matches(Attributes.from("(msg=\\5cqq)"));
+
+        //Broken opaque value escape sequence
+        try
+        {
+            parser
+                    .parse("(msg=\\)")
+                    .matches(Attributes.from("(msg=\\FF\\01\\02\\03)"));
+            assert false;
+        }
+        catch (ServiceLocationException sle) {}
+
+        try
+        {
+            parser
+                    .parse("(msg=\\xx)")
+                    .matches(Attributes.from("(msg=\\FF\\01\\02\\03)"));
+            assert false;
+        }
+        catch (ServiceLocationException sle) {}
+
+        try
+        {
+            parser
+                    .parse("(msg=xab)")
+                    .matches(Attributes.from("(msg=\\FF\\01\\02\\03)"));
+            assert false;
+        }
+        catch (ServiceLocationException sle) {}
+    }
+
+    @Test
+    public void testToString()
+    {
+        FilterParser parser = new FilterParser();
+
+        String expression = "";
+        Filter filter = parser.parse(expression);
+        assert expression.equals(filter.asString());
+
+        expression = "(a=1)";
+        filter = parser.parse(expression);
+        assert expression.equals(filter.asString());
+
+        expression = "(&(a=1)(b=false))";
+        filter = parser.parse(expression);
+        assert expression.equals(filter.asString());
+
+        expression = "(&(!(b=false))(a=1))";
+        filter = parser.parse(expression);
+        assert expression.equals(filter.asString());
+
+        expression = "(|(name=name*)(&(!(b=true))(a=2)))";
+        filter = parser.parse(expression);
+        assert expression.equals(filter.asString());
+    }
+
+}
